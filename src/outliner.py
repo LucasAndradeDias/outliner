6fb06ca1c24dc1e621382c5d29fa56f0185eea9d
LucasAndradeDias <lucasan1234565@gmail.com>
2023-09-28 17:00:33 -0300

    Added opcional arguments to running object

--- src/outliner.py
+++ src/outliner.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import ast
 
 from pathlib import Path
 from trace import Trace
@@ -11,20 +12,24 @@ def parser_arguments():
     parser.add_argument("--file_path", required=True)
     parser.add_argument("--object_type", required=False)
     parser.add_argument("--object_name", type=str, required=True)
-    parser.add_argument("--object_args", action="append", type=list, required=False)
+    parser.add_argument("--object_args", nargs="+", type=str, required=False)
     return parser.parse_args()
 
 
 def check_args(args):
     if not Path(args.file_path).is_file():
-        raise ("Not a valid file")
+        raise Exception("Not a valid file")
     if not ".py" in args.file_path:
-        raise ("Not a python file")
+        raise Exception("Not a python file")
 
 
 def main(args):
     trace = Trace()
-    trace.run(args.file_path, args.object_name, args.object_args)
+    if args.object_args:
+        trace.run(args.file_path, args.object_name, args.object_args[0].split())
+    else:
+        trace.run(args.file_path, args.object_name)
+
     display_class = Display(trace.detailed_data, trace.functions_flow)
     display_class.tree()
 
