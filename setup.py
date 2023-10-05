b187cba51003827016913e881ab2eeeb0c98a77d
LucasAndradeDias <lucasan1234565@gmail.com>
2023-10-05 15:50:25 -0300

    Updated setup.py for release in pypi

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
 
 
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
 setup(
-    name="outliner",
+    name="outliner-tracer",
     description="A tracer for compiled objects",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     license="MIT",
     url="https://github.com/LucasAndradeDias/outliner",
     version="1.3.0a1",
-    keywords="Tracing complex callable objects",
+    keywords=["tracer","outliner","tracing","complex","objects"],
     author="Lucas A. Dias",
     author_email="lucasan1234565@gmail.com",
+    long_description=long_description,
+    long_description_content_type='text/markdown',  
     entry_points={
         "console_scripts": [
             "outliner = outliner.cli:main",
