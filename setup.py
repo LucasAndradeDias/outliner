c0fc7dd1549883f2417256ffa66e745f6dcde135
LucasAndradeDias <lucasan1234565@gmail.com>
2023-10-05 14:04:22 -0300

    Updated version

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     description="A tracer for compiled objects",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    license='MIT',
+    license="MIT",
     url="https://github.com/LucasAndradeDias/outliner",
-    version="1.2.0a1",
-    keywords='Tracing complex callable objects',
-    author='Lucas A. Dias',
-    author_email='lucasan1234565@gmail.com',
+    version="1.3.0a1",
+    keywords="Tracing complex callable objects",
+    author="Lucas A. Dias",
+    author_email="lucasan1234565@gmail.com",
     entry_points={
         "console_scripts": [
             "outliner = outliner.cli:main",
