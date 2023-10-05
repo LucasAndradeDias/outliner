1bd942797c7651851d5b1b6b115b4cf7c4a4a0ab
LucasAndradeDias <lucasan1234565@gmail.com>
2023-10-05 11:03:36 -0300

    Adding more arguments to the user on display data

--- src/outliner/cli.py
+++ src/outliner/cli.py
@@ -1,13 +1,33 @@
 import argparse
-from .outliner import Outliner  # Import functions from your library
+from .outliner import Outliner
 
 
 def parser_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file_path", required=True)
-    parser.add_argument("--object_type", required=False)
-    parser.add_argument("--object_name", type=str, required=True)
-    parser.add_argument("--object_args", action="append", type=str, required=False)
+    parser.add_argument(
+        "--file_path", help="Path to the file you want to trace", required=True
+    )
+    parser.add_argument(
+        "--object_name",
+        help="The name of the object you want to trace",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--object_args",
+        help="A list with the argumments required to run the object",
+        action="append",
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "--display",
+        "-d",
+        help="Select the type of display you want",
+        choices=["tree", "detailed_data"],
+        default="tree",
+    )
+
     return parser.parse_args()
 
 
@@ -18,6 +38,7 @@ def main():
         args.file_path,
         args.object_name,
         args.object_args[0].split() if args.object_args else None,
+        args.display,
     )
 
     instance_class.run()
