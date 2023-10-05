43c633cfff1059169b89de7452c66c03c43ddf4c
LucasAndradeDias <lucasan1234565@gmail.com>
2023-10-05 11:51:32 -0300

    Updated setup.py to release the lib

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(
     package_dir={"": "src"},
     license='MIT',
     url="https://github.com/LucasAndradeDias/outliner",
+    version="1.2.0a1",
+    keywords='Tracing complex callable objects',
+    author='Lucas A. Dias',
+    author_email='lucasan1234565@gmail.com',
     entry_points={
         "console_scripts": [
             "outliner = outliner.cli:main",
