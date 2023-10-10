b6eba883b612f925a27f3c4169c4daa043ddcc2a
LucasAndradeDias <lucasan1234565@gmail.com>
2023-10-10 16:31:37 -0300

    Added first edition of outliner

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 
-
-with open('README.md', 'r') as readme_file:
+with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 setup(
@@ -12,12 +11,12 @@ setup(
     package_dir={"": "src"},
     license="MIT",
     url="https://github.com/LucasAndradeDias/outliner",
-    version="1.3.0a1",
-    keywords=["tracer","outliner","tracing","complex","objects"],
+    version="1.0.0",
+    keywords=["tracer", "outliner", "tracing", "complex", "objects"],
     author="Lucas A. Dias",
     author_email="lucasan1234565@gmail.com",
     long_description=long_description,
-    long_description_content_type='text/markdown',  
+    long_description_content_type="text/markdown",
     entry_points={
         "console_scripts": [
             "outliner = outliner.cli:main",
