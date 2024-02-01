ace612919af5d55cc901226220fc2ff1b9496ed3
LucasAndradeDias <lucasan1234565@gmail.com>
2024-01-31 22:46:14 -0300

    Updated utils modules

--- src/outliner/utils/util_modules.py
+++ src/outliner/utils/util_modules.py
@@ -1,4 +1,3 @@
-from importlib import util
 from pathlib import Path
 
 
@@ -8,17 +7,4 @@ def find_module_path(name: str, root_path: Path) -> Path:
         Returns Posix
     """
     found_modules = list(root_path.glob(f"**/{name}.py"))
-    return found_modules[0] if any(found_modules) else None
-
-def add_import_module_to_namespace(modules: set):
-    """
-    Add all local modules used in script to the global namespace
-    arguments:
-    @param cls: script_obj
-    """
-    for import_module in modules:
-        module_name = import_module[0]
-        module_path = import_module[1]
-        module_spec = util.spec_from_file_location(module_name,str(module_path))
-        module_obj = util.module_from_spec(module_spec)
-        module_spec.loader.exec_module(module_obj)        
+    return str(found_modules[0].parent) if any(found_modules) else None
