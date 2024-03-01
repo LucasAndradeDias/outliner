566a6aea5884c7cef9e0be036d175fb6274ba54c
Lucas Andrade Dias <lucaspersonal@MacBook-Pro-de-ti.local>
2024-02-29 22:42:28 -0300

    Updated trace unit tests

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -7,6 +7,8 @@ from src.outliner import Trace
 from unittest.mock import patch
 from io import StringIO
 
+from src.outliner.views import ModuleObject, RunningObject
+
 
 class TestTrace(unittest.TestCase):
     def setUp(self):
@@ -20,67 +22,41 @@ class TestTrace(unittest.TestCase):
         )
         self.parser.functions_flow = collections.OrderedDict()
 
-    def test_run_file_positive_input(self):
+    def test_trace_obj_positive_input(self):
         """
-        Given a valid module path and a callable function "test",
-        When Trace.run_file is called with these parameters,
-        Then it should trace the function and produce the expected trace result.
+        Given a valid running obj
+        When trace.trace_obj is called with this parameter
+        Then the running_obj should be executed and the class property updated
         """
-        module_path = Path(self.mock_path + "/module_testing_1.py")
-        self.parser.run_file(module_path, "test()")
+        module_obj = ModuleObject(Path(self.mock_path + "/module_testing_1.py"))
+        module_running_obj = RunningObject(module_obj, "test()")
+        self.parser.trace_obj(module_running_obj)
         expected_trace = r"Trace object\nrunned objects:\n    __init__ + \n    func1 + \n    func2 + \n    "
         self.assertEqual(expected_trace, repr(self.parser))
 
-    def test_run_file_positive_input_with_parameters(self):
+    def test_trace_obj_negative_no_running_obj(self):
         """
-        Given a valid module path, a callable function "numberToIp",
-        and function parameters ["000"],
-        When Trace.run_file is called with these parameters,
-        Then it should trace the function and produce the expected trace result.
-        """
-        module_path = Path(self.mock_path + "/module_testing_2.py")
-        self.parser.run_file(module_path, "numberToIp('000')")
-
-        expected_trace = r"Trace object\nrunned objects:\n    numberToIp + \n    "
-
-        self.assertEqual(expected_trace, repr(self.parser))
-
-    def test_trace_run_file_negative_no_module(self):
-        """
-        Given an invalid module path,
+        Given an invalid RunningObject,
         When Trace.run_file is called with these parameters,
         Then it should raise an exception with an appropriate error message.
         """
 
         with self.assertRaises(Exception) as error:
-            self.parser.run_file(Path("not_valid_path"), "object")
+            self.parser.trace_obj("Not valid")
         self.assertTrue(
-            "'NoneType' object has no attribute 'loader" in str(error.exception)
+            "Given object 'Not valid' is not callable." in str(error.exception)
         )
-
-    def test_trace_run_file_file_negative_non_callable_object(self):
-        """
-        Given a valid module path and a non-callable object "non_callable",
-        When Trace.run_file is called with these parameters,
-        Then it should raise an exception with an appropriate error message.
-        """
-        module_path = (
-            self.mock_path + "/module_testing_2.py"
-        )  # Contains a non-callable object
-        with self.assertRaises(Exception) as error:
-            self.parser.run_file(module_path, "non_callable")
-            self.assertEqual(
-                "Object 'non_callable' not found in module" in str(error.exception)
-            )
 
     def test_trace_run_file_positive_multiple_runs(self):
         """
         Given a valid module path and a callable function "test",
-        When Trace.run_file is called twice with these parameters,
+        When Trace.trace_obj is called twice with these parameters,
         Then it should trace the functions individually and produce the expected trace result for each run.
         """
-        module_path = Path(self.mock_path + "/module_testing_1.py")
-        self.parser.run_file(module_path, "test()")
-        self.parser.run_file(module_path, "test2()")
+        module_path = ModuleObject(Path(self.mock_path + "/module_testing_1.py"))
+        module_running_obj_1 = RunningObject(module_path, "test()")
+        module_running_obj_2 = RunningObject(module_path, "test2()")
+        self.parser.trace_obj(module_running_obj_1)
+        self.parser.trace_obj(module_running_obj_2)
         expected_trace = r"Trace object\nrunned objects:\n    __init__ + \n    func1 + \n    func2 + \n    test2 + \n    "
         self.assertEqual(expected_trace, repr(self.parser))
